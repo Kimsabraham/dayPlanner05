513b2defdf5a263d4df8269f3bc7db3f967a148f
Abraham Kim <krnpride730@yahoo.com>
2021-03-20 19:09:42 -0400

    added more detail in read me and comments

--- Logic/script.js
+++ Logic/script.js
@@ -1,7 +1,16 @@
+// uses moment to track date and time
 $(document).ready(function () {
   var currentDay = $("#currentDay");
   currentDay.text(moment().format("dddd, MMMM Do"));
+  // this function takes the date and it to the ID's in the HTML
+  const hour = new Date().getHours();
+  let twoPm = $("");
+  //  starting at 2PM
+  const twoPmInt = $("#14").attr("id");
 
+  twoPm.addClass("present");
+
+  // on click saves button into local storage event
   $(".btn").on("click", function () {
     var inputValue = $(this).siblings(".description").val();
     console.log(inputValue);
@@ -10,6 +19,7 @@ $(document).ready(function () {
     localStorage.setItem(id, inputValue);
   });
 
+  //   pairs id with key values in console.
   function getLocalStorage() {
     $(".time-block").each(function () {
       var id = $(this).children(".hour").attr("id");
