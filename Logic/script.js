3f975dda8d5344800129496e9665efaf3780554e
Abraham Kim <krnpride730@yahoo.com>
2021-03-23 12:12:42 -0400

    fixed local storage and color component with time

--- Logic/script.js
+++ Logic/script.js
@@ -3,12 +3,6 @@ $(document).ready(function () {
   var currentDay = $("#currentDay");
   currentDay.text(moment().format("dddd, MMMM Do"));
   // this function takes the date and it to the ID's in the HTML
-  const hour = new Date().getHours();
-  let twoPm = $("");
-  //  starting at 2PM
-  const twoPmInt = $("#14").attr("id");
-
-  twoPm.addClass("present");
 
   // on click saves button into local storage event
   $(".btn").on("click", function () {
@@ -19,11 +13,21 @@ $(document).ready(function () {
     localStorage.setItem(id, inputValue);
   });
 
-  //   pairs id with key values in console.
+  //   stores and calls users info into local storage
   function getLocalStorage() {
+    var currentHour = moment().hour();
+    console.log(currentHour);
     $(".time-block").each(function () {
       var id = $(this).children(".hour").attr("id");
-      $(".description").val(localStorage.getItem(id));
+      // color codes based on hour
+      $(this).children(".description").val(localStorage.getItem(id));
+      if (currentHour > id) {
+        $(this).addClass("past");
+      } else if (currentHour < id) {
+        $(this).addClass("future");
+      } else {
+        $(this).addClass("present");
+      }
     });
   }
   getLocalStorage();
